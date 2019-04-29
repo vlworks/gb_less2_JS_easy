f869eed4c10d33963aebc5c1bd7e90ae06e3e610
VladL <vlad169@yandex.ru>
2019-04-29 18:27:58 +0300

    работа над ошибками

--- leson2.js
+++ leson2.js
@@ -44,23 +44,53 @@ if (a >= 0 && b >= 0) {
 /* Четвертое задание */
 console.log('=== Четвертое задание ===');
 
-var a = 5;
+var a = +prompt('введите значение от 1 до 15');
 
 switch(a){
     case 1:
-    console.log('1');
+    alert('1');
     break;
     case 2:
-    console.log('2');
+    alert('2');
     break;
     case 3:
-    console.log('3');
+    alert('3');
     break;
     case 4:
-    console.log('4');
+    alert('4');
     break;
     case 5:
-    console.log('5');
+    alert('5');
+    break;
+    case 6:
+    alert('6');
+    break;
+    case 7:
+    alert('7');
+    break;
+    case 8:
+    alert('8');
+    break;
+    case 9:
+    alert('9');
+    break;
+    case 10:
+    alert('10');
+    break;
+    case 11:
+    alert('11');
+    break;
+    case 12:
+    alert('12');
+    break;
+    case 13:
+    alert('13');
+    break;
+    case 14:
+    alert('14');
+    break;
+    case 15:
+    alert('15');
     break;
 }
 
