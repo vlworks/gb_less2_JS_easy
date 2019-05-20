a679c86950474e4c5c6247ef121baafe616683ac
VladL <vlad169@yandex.ru>
2019-05-20 14:01:16 +0300

    добавил появление модального окна

--- lesson6/main.js
+++ lesson6/main.js
@@ -5,41 +5,57 @@ var product = [
         name: 'Рубашка мужская',
         quantity: 2,
         price: 1000,
+        img: ['img/shirt1.jpg', 'img/shirt2.jpg', 'img/shirt3.jpg'],
+        imgDefault: 'img/shirt1.jpg',
     },
     {
         name: 'Рубашка женская',
         quantity: 3,
         price: 1500,
+        img: [],
+        imgDefault: 'https://i1.ytimg.com/sh/nFCxMKeqg8k/showposter.jpg',
     },
     {
         name: 'Зонтик',
         quantity: 8,
         price: 700,
+        img: [],
+        imgDefault: 'https://i1.ytimg.com/sh/nFCxMKeqg8k/showposter.jpg',
     },
     {
         name: 'Носки',
         quantity: 2,
         price: 1000,
+        img: [],
+        imgDefault: 'https://i1.ytimg.com/sh/nFCxMKeqg8k/showposter.jpg',
     },
     {
         name: 'Шапка',
         quantity: 3,
         price: 1500,
+        img: [],
+        imgDefault: 'https://i1.ytimg.com/sh/nFCxMKeqg8k/showposter.jpg',
     },
     {
         name: 'Перчатки',
         quantity: 8,
         price: 700,
+        img: [],
+        imgDefault: 'https://i1.ytimg.com/sh/nFCxMKeqg8k/showposter.jpg',
     },
     {
         name: 'Лыжи',
         quantity: 3,
         price: 1500,
+        img: [],
+        imgDefault: 'https://i1.ytimg.com/sh/nFCxMKeqg8k/showposter.jpg',
     },
     {
         name: 'Парашют',
         quantity: 8,
         price: 700,
+        img: [],
+        imgDefault: 'https://i1.ytimg.com/sh/nFCxMKeqg8k/showposter.jpg',
     },
 ];
 
@@ -58,9 +74,13 @@ for(var i = 0; i < product.length; i++){
     $catalog.appendChild($card);
     
     var $imgCard = document.createElement('img');
-    $imgCard.src = 'https://i1.ytimg.com/sh/nFCxMKeqg8k/showposter.jpg'; // можно использовать массив с ссылками на картинки
+    $imgCard.src = product[i].imgDefault;
     $imgCard.classList.add('card__img');
     $card.appendChild($imgCard);
+    // добавляем атрибут счетчика массива к картинке
+    $imgCard.setAttribute('name', product[i].name);
+    //слушаем клики
+    $imgCard.addEventListener('click', handleImgClick);
 
     var $cardTitle = document.createElement('p');
     $cardTitle.textContent = product[i].name;
@@ -78,7 +98,7 @@ for(var i = 0; i < product.length; i++){
     $cardLink.setAttribute('name', product[i].name);
     
     $card.appendChild($cardLink);
-
+    //слушаем клики
     $cardLink.addEventListener('click', handleBuyClick);
 }
 
@@ -132,6 +152,68 @@ $bSum.textContent = message;
 
         basket.push(productName, productPrice);
     }
+
+// работа с изображениями
+
+    function handleImgClick(event){
+        // открываем окно
+        var $modal = document.createElement('div');
+        $modal.classList.add('catalog__modal');
+        $catalog.appendChild($modal);
+
+        // создаем кнопку закрыть
+        var $closeModal = document.createElement('div');
+        $closeModal.classList.add('catalog__modal_close');
+        $closeModal.textContent = 'Закрыть';
+        $modal.appendChild($closeModal);
+
+        // создаем навигацию и центральное окно с изображением
+
+        var $modalFlex = document.createElement('div');
+        $modalFlex.classList.add('modal__flex');
+        $modal.appendChild($modalFlex);
+
+        var $modalFlexLeft = document.createElement('div');
+        $modalFlexLeft.classList.add('modalFlex__left');
+        $modalFlexLeft.textContent = '<';
+        $modalFlex.appendChild($modalFlexLeft);
+
+        var $modalFlexCenter = document.createElement('div');
+        $modalFlexCenter.classList.add('modalFlex__center');
+        $modalFlex.appendChild($modalFlexCenter);
+
+        var $modalFlexRight = document.createElement('div');
+        $modalFlexRight.classList.add('modalFlex__right');
+        $modalFlexRight.textContent = '>';
+        $modalFlex.appendChild($modalFlexRight);
+
+        //созадем картинку
+
+        var $modalImg = document.createElement('img');
+        $modalImg.classList.add('modal__img');
+        $modalImg.setAttribute('src', 'https://i1.ytimg.com/sh/nFCxMKeqg8k/showposter.jpg');
+        $modalFlexCenter.appendChild($modalImg);
+
+        //слушаем кнопку закрыть
+        $closeModal.addEventListener('click', handleCloseClick);
+
+        //функция закрытия модального окна
+        function handleCloseClick(){
+            $catalog.removeChild($modal);
+        }
+        
+    }
+
+
+
+    // <div class="catalog__modal">
+    //             <div class="catalog__modal_close">Закрыть</div>
+    //             <div class="modal__flex">
+    //                 <div class="modalFlex__left"><</div>
+    //                 <div class="modalFlex__center"><img class="modal__img" src="https://i1.ytimg.com/sh/nFCxMKeqg8k/showposter.jpg" alt=""></div>
+    //                 <div class="modalFlex__right">></div>
+    //             </div>
+    //         </div>
 
 
 
